a58241030be9000ba03f4947302ff4fafba42eb0
NiveditaPatle <nivepatle@gmail.com>
2025-04-15 16:49:10 +0530

    update link

--- src/pages/VideoRoom.jsx
+++ src/pages/VideoRoom.jsx
@@ -1,21 +1,16 @@
-import { ZegoUIKitPrebuilt } from "@zegocloud/zego-uikit-prebuilt";
-import React from "react";
-import { useParams } from "react-router-dom";
+import { ZegoUIKitPrebuilt } from '@zegocloud/zego-uikit-prebuilt';
+import React from 'react'
+import { useParams } from 'react-router-dom'
 
 function VideoRoom() {
-  let { roomID } = useParams();
+
+  let {roomID} = useParams();
 
   const myMetting = async (element) => {
     // generate Kit Token
     const appID = 135264447;
     const serverSecret = "b5bf274e57f80c78a5b7ee67032c39f1";
-    const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(
-      appID,
-      serverSecret,
-      roomID,
-      Date.now().toString(),
-      "NP"
-    );
+    const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(appID,serverSecret,roomID,Date.now().toString(),"NP")
 
     // Create instance object from Kit Token.
     const zp = ZegoUIKitPrebuilt.create(kitToken);
@@ -24,27 +19,21 @@ function VideoRoom() {
       container: element,
       sharedLinks: [
         {
-          name: "Copy link",
-          url:
-            window.location.protocol +
-            "//" +
-            window.location.host +
-            window.location.pathname +
-            "?roomID=" +
-            roomID,
+          name: 'Copy link',
+          url:`https://video-calling-web-app-seven.vercel.app/room/${roomID}`
         },
       ],
       scenario: {
         mode: ZegoUIKitPrebuilt.GroupCall, 
       },
     });
-  };
+  }
 
   return (
     <div>
-      <div ref={myMetting} />
+      <div ref={myMetting}/>
     </div>
-  );
+  )
 }
 
-export default VideoRoom;
+export default VideoRoom
